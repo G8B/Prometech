0b1b0cf26e669f2350d18e2835a63c1fc93a3186
Marin Godechot <marin.godechot@gmail.com>
2018-03-09 13:08:05 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,3 @@
+<header class="header">
+    <a href="#"><img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" /></a>
+</header>
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Prometech</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" >
+</head>
+<body>
+<?php include("header.php"); ?>
+</body>
+</html>
