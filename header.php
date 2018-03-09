10f00b6ee0eb07d8338c4b047afb26c4ac90afde
Marin Godechot <marin.godechot@gmail.com>
2018-03-09 15:24:29 +0100

    add white bars button

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
 <div class="header">
-    <a href="#" id="logo"><img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" /></a>
+    <a href="#" id="logo">
+        <img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" />
+    </a>
+    <button id="bars"><i class="fa fa-bars" id="icon-bars"></i></button>
 </div>
