a4d9e7072601e798301afe6147c1f3685b2981c2
Marin Godechot <marin.godechot@gmail.com>
2018-03-09 13:50:32 +0100

    make logo fit to header

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
-<header class="header">
-    <a href="#"><img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" /></a>
-</header>
+<div class="header">
+    <a href="#" id="logo"><img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" /></a>
+</div>
