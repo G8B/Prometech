f9acc4a6aa3d817b78b05b3c70fbe21ab9e729a1
Marin Godechot <marin.godechot@gmail.com>
2018-03-10 18:40:01 +0100

    add username icon and change buttons to links

--- header.php
+++ header.php
@@ -2,5 +2,9 @@
     <a href="#" id="logo">
         <img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" />
     </a>
-    <button id="bars"><i class="fa fa-bars" id="icon-bars"></i></button>
+    <a id="bars" href="#"><i class="fa fa-bars" id="icon-bars"></i></a>
+    <a id="userProfile" href="#">
+        <i class="fa fa-user" id="icon-user"></i> <br>
+        <p id="username">Username</p>
+    </a>
 </div>
