e0300819c53a86bacc81db50626f2e544bb4a7bf
Marin Godechot <marin.godechot@gmail.com>
2018-03-11 02:42:46 +0100

    sidebar toggle animation

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
     <a href="#" id="logo">
         <img src="https://image.ibb.co/fjUEdS/logoprom_Side_WHITE.png" alt="logoprom Side WHITE" border="0" id="whiteLogo" />
     </a>
-    <a id="bars" href="#"><i class="fa fa-bars" id="icon-bars"></i></a>
-    <a id="userProfile" href="#">
+    <a id="bars-button" href="#"><i class="fa fa-bars" id="icon-bars"></i></a>
+    <a id="userProfile-button" href="#">
         <i class="fa fa-user-circle" id="icon-user"></i> <br>
         <p id="username">Username</p>
     </a>
--- index.php
+++ index.php
@@ -6,11 +6,23 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="styles.css" >
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 </head>
 <body>
 <?php include("header.php"); ?>
 
 <?php include("sidebar.php"); ?>
 
+<!-- Sidebar toggle -->
+<script>
+    $(function() {
+        $( "#bars-button" ).click(function() {
+            $( "#sidebar" ).toggle('slide', {
+                direction: 'left'
+            });
+        });
+    });
+</script>
 </body>
 </html>
