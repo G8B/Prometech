6cab41a4fd48f28275a4637617e2d3d3e6990747
Marin Godechot <marin.godechot@gmail.com>
2018-03-12 15:01:21 +0100

    add pure CSS accordion

--- pages/user-dashboard.php
+++ pages/user-dashboard.php
@@ -2,3 +2,42 @@
     <a class="dashboard-nav-link" href="#"><i class="fas fa-sliders-h"></i></a>
     <a class="dashboard-nav-link" href="#"><i class="fas fa-chart-bar"></i></a>
 </div>
+
+<div class="user-dashboard">
+    <div class="user-house-dashboard">
+        <h1 class="user-house-name">Logement 1</h1>
+        <div class="accordion-tab">
+            <input id="accordion-tab-one" type="radio" name="accordion" checked>
+            <label for="accordion-tab-one">Label One</label>
+            <div class="accordion-tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
+                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
+                    adipisci eum? Saepe, itaque.</p>
+            </div>
+        </div>
+        <div class="accordion-tab">
+            <input id="accordion-tab-two" type="radio" name="accordion">
+            <label for="accordion-tab-two">Label Two</label>
+            <div class="accordion-tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
+                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
+                    adipisci eum? Saepe, itaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
+                    architecto, explicabo perferendis
+                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
+                    adipisci eum? Saepe, itaqueLorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
+                    architecto, explicabo perferendis
+                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
+                    adipisci eum? Saepe, itaque</p>
+            </div>
+        </div>
+        <div class="accordion-tab">
+            <input id="accordion-tab-three" type="radio" name="accordion">
+            <label for="accordion-tab-three">Label Three</label>
+            <div class="accordion-tab-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
+                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
+                    adipisci eum? Saepe, itaque.</p>
+            </div>
+        </div>
+    </div>
+</div>
