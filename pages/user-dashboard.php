0a5306f4b291d99e769d09ea59a686f3a19e91f8
Marin Godechot <marin.godechot@gmail.com>
2018-03-12 16:04:24 +0100

    add product grid in tab-content

--- pages/user-dashboard.php
+++ pages/user-dashboard.php
@@ -10,33 +10,36 @@
             <input id="accordion-tab-one" type="radio" name="accordion" checked>
             <label for="accordion-tab-one">Salon</label>
             <div class="accordion-tab-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
-                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
-                    adipisci eum? Saepe, itaque.</p>
+                <div class="product-grid">
+                    <div class="product-box">Capteur 1</div>
+                    <div class="product-box">Actionneur 1</div>
+                    <div class="product-box">Capteur 2</div>
+                    <div class="product-box">Actionneur 2</div>
+                </div>
             </div>
         </div>
         <div class="accordion-tab">
             <input id="accordion-tab-two" type="radio" name="accordion">
             <label for="accordion-tab-two">Chambre Parents</label>
             <div class="accordion-tab-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
-                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
-                    adipisci eum? Saepe, itaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
-                    architecto, explicabo perferendis
-                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
-                    adipisci eum? Saepe, itaqueLorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
-                    architecto, explicabo perferendis
-                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
-                    adipisci eum? Saepe, itaque</p>
+                <div class="product-grid">
+                    <div class="product-box">Capteur 1</div>
+                    <div class="product-box">Actionneur 1</div>
+                    <div class="product-box">Capteur 2</div>
+                    <div class="product-box">Actionneur 2</div>
+                </div>
             </div>
         </div>
         <div class="accordion-tab">
             <input id="accordion-tab-three" type="radio" name="accordion">
             <label for="accordion-tab-three">Chambre Enfants</label>
             <div class="accordion-tab-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, architecto, explicabo perferendis
-                    nostrum, maxime impedit atque odit sunt pariatur illo obcaecati soluta molestias iure facere dolorum
-                    adipisci eum? Saepe, itaque.</p>
+                <div class="product-grid">
+                    <div class="product-box">Capteur 1</div>
+                    <div class="product-box">Actionneur 1</div>
+                    <div class="product-box">Capteur 2</div>
+                    <div class="product-box">Actionneur 2</div>
+                </div>
             </div>
         </div>
     </div>
