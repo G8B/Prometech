41b28826a8ecf9b591911c29758aaf07f7dc0cb9
Marin Godechot <marin.godechot@gmail.com>
2018-03-12 13:43:42 +0100

    add footer links in the sidebar

--- pages/sidebar.php
+++ pages/sidebar.php
@@ -3,4 +3,10 @@
     <a class="side-button" href="#">Gestion utilisateurs</a>
     <a class="side-button" href="#">Gestion logements</a>
     <a class="side-button" href="#">Tableau de bord</a>
+
+    <ul id="link-footer">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">CGU</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
 </div>
