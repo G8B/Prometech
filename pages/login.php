cbf0439bcc8f39a0dd830627d2c2db5f19b149ee
danykh <dany.khalil97@gmail.com>
2018-03-14 11:07:21 +0100

    add login and sign up files

---
+++ pages/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="test.css" />
+</head>
+<body>
+<center>
+
+    <p>
+        <img src = "original-logos_2018_Feb_4074-5a815ae404ef4.png"/>
+    </p>
+    <form method="post" action="PremierePage.php">
+        <p id="email">
+            <label for="email"></label>
+            <input type="email" name="email" id="email" placeholder="Email" size="25" style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"  />
+        </p>
+        <p id="mdp">
+            <label for="password"></label>
+            <input type="password" name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" size="25" style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;" />
+        </p>
+        <p >
+            <input id="envoyer" type="submit" value="Se connecter >" style="color:black; background-color:#ED7F10; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;" />
+        </p>
+    </form>
+
+    <hr width="15%" color="black">
+
+
+    <p >
+
+    <form   action="inscription.php">
+        <input id = "inscription" type="submit" value="S'inscrire >"  style="color:black; background-color:#ED7F10; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;margin-bottom: 340px;"/>
+</center>
+
+
+</form>
+</p>
+
+
+</body>
+</html>
