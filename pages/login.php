4f36e0e347d7e30ff30a40ed35372dfea75f18fc
Marin Godechot <marin.godechot@gmail.com>
2018-03-14 13:22:41 +0100

    add local images

--- pages/login.php
+++ pages/login.php
@@ -5,9 +5,8 @@
 </head>
 <body>
 <center>
-
     <p>
-        <img src="original-logos_2018_Feb_4074-5a815ae404ef4.png"/>
+        <img src="../ressources/original-logos_2018_Feb_4074-5a815ae404ef4.png"/>
     </p>
     <form method="post" action="signInTreatment.php">
         <p id="email">
@@ -34,11 +33,8 @@
     <form action="sign-up.php">
         <input id="inscription" type="submit" value="S'inscrire >"
                style="color:black; background-color:#ED7F10; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;margin-bottom: 340px;"/>
+    </form>
 </center>
-
-
-</form>
-</p>
 
 
 </body>
