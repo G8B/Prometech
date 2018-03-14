c28a4954054e6953d37337be64ef5d461d2c15d9
Marin Godechot <marin.godechot@gmail.com>
2018-03-14 21:26:10 +0100

    changed deprecated <center> for a text-align css property in login.php

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="../styles/sign-up.css"/>
 </head>
 <body>
-<center>
+<div class="form">
     <p>
         <img src="../ressources/original-logos_2018_Feb_4074-5a815ae404ef4.png"/>
     </p>
@@ -34,7 +34,7 @@
         <input id="inscription" type="submit" value="S'inscrire >"
                style="color:black; background-color:#ED7F10; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;margin-bottom: 340px;"/>
     </form>
-</center>
+</div>
 
 
 </body>
