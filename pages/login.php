61b56adf9f8a9a508fb5294c294d29e939c5fe0a
Marin Godechot <marin.godechot@gmail.com>
2018-03-16 11:06:31 +0100

    css cleanup

--- pages/login.php
+++ pages/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="../styles/login.css"/>
     <link rel="stylesheet" href="../styles/sign-up.css"/>
 </head>
 <body>
@@ -11,17 +12,14 @@
     <form method="post" action="signInTreatment.php">
         <p id="email">
             <label for="email"></label>
-            <input type="email" name="email" id="email" placeholder="Email" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="email" name="email" id="email" placeholder="Email" size="25"/>
         </p>
         <p id="mdp">
             <label for="password"></label>
-            <input type="password" name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="password" name="mot_de_passe" id="mot_de_passe" placeholder="Mot de passe" size="25"/>
         </p>
         <p>
-            <input id="envoyer" type="submit" value="Se connecter >"
-                   style="color:black; background-color:#E29845; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;"/>
+            <input class="form-button" type="submit" value="Se connecter >"/>
         </p>
     </form>
 
@@ -31,8 +29,7 @@
     <p>
 
     <form action="sign-up.php">
-        <input id="inscription" type="submit" value="S'inscrire >"
-               style="color:black; background-color:#E29845; border-radius: 5px; height: 25px;width: 200px; font-weight: bold;margin-bottom: 340px;"/>
+        <input class="form-button" type="submit" value="S'inscrire >"/>
     </form>
 </div>
 
--- pages/sign-up.php
+++ pages/sign-up.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <link rel="stylesheet" href="../styles/sign-up.css"/>
+    <link rel="stylesheet" href="../styles/login.css"/>
 </head>
 <body>
 <div class="form">
@@ -12,50 +13,42 @@
         </p>
         <p>
             <label for="nom"></label>
-            <input type="text" name="nom" id="nom" placeholder="Nom" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="text" name="nom" id="nom" placeholder="Nom" size="25"/>
         </p>
         <p>
             <label for="prenom"></label>
-            <input type="text" name="prenom" id="prenom" placeholder="Prénom" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="text" name="prenom" id="prenom" placeholder="Prénom" size="25"/>
         </p>
         <p>
             <label for="pseudo"></label>
-            <input type="email" name="email" id="email" placeholder="Email" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="email" name="email" id="email" placeholder="Email" size="25"/>
         </p>
         <p>
             <label for="adresse"></label>
-            <input type="text" name="adresse" id="adresse" placeholder="Adresse" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="text" name="adresse" id="adresse" placeholder="Adresse" size="25"/>
         </p>
         <p>
             <label for="password"></label>
-            <input type="password" name="mot de passe" id="mot de passe" placeholder="Mot de passe" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="password" name="mot de passe" id="mot de passe" placeholder="Mot de passe" size="25"/>
         </p>
         <p>
             <label for="password"></label>
-            <input type="password" name="password" id="password" placeholder="Confirmer mot de passe" size="25"
-                   style="color:black; background-color:#FFCB60; border-radius: 5px; height: 20px;"/>
+            <input type="password" name="password" id="password" placeholder="Confirmer mot de passe" size="25"/>
         </p>
-        <p>
+        <p class="form-checkbox">
             <label for="nom"></label>
             <input type="checkbox" name="choix1" id="choix1"/> <label for="choix1"> J'utilise des produits
                 Domisep </label> <br/>
             <input type="checkbox" name="choix2" id="choix2"/> <label for="choix2"> Je gère un immeuble </label>
         </p>
         <form action="signupTreatment.php">
-            <input id='inscription' type="submit" value="S'inscrire >"
-                   style="color:black; background-color:#E29845; border-radius: 5px; height: 25px;width: 200px; font-weight: bold; margin-bottom: 10px;"/>
+            <input id='inscription' class="form-button" type="submit" value="S'inscrire >"/>
         </form>
 
 
         <hr width="15%" color="black">
 
         <form action="../pages/login.php">
-            <input id='deja' type="submit" value="Déjà inscrit ? "
-                   style="color:black; background-color:#E29845; border-radius: 5px; height: 25px;width: 200px; font-weight: bold; margin-top: 10px; margin-bottom: 110px;"/>
+            <input id='deja' class="form-button" type="submit" value="Déjà inscrit ? "/>
 
 </div>
