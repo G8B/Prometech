1cf3d0f30969d81be9821856866d13d3b42f5c94
Marin Godechot <marin.godechot@gmail.com>
2018-03-16 11:34:37 +0100

    fix nav-link in user-dashboard

--- pages/user-dashboard.php
+++ pages/user-dashboard.php
@@ -1,5 +1,5 @@
 <div id="dashboard-nav">
-    <a class="dashboard-nav-link" href="#"><i class="fas fa-sliders-h"></i></a>
+    <a class="dashboard-nav-link current-dashboard" href="#"><i class="fas fa-sliders-h"></i></a>
     <a class="dashboard-nav-link" href="#"><i class="fas fa-chart-bar"></i></a>
 </div>
 
