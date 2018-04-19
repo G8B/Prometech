c4946bfb518b2393424f245c59e578c7421dc9c4
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-19 16:46:06 +0200

    ad edition-compte treatment

--- model/edition_compte-treatment.php
+++ model/edition_compte-treatment.php
@@ -1,7 +1,20 @@
-<?php 
+<?php
 $bdd = new PDO('mysql:host=localhost;dbname=promethec' , 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$req = $bdd->prepare('INSERT
-        INTO utilisateurs(nom, prenom, email, password, statutClient, statutGestionnaire, statutAdmin, statutSubordonne)
-        VALUES(:nom, :prenom, :email, :password, :statutClient, :statutGestionnaire, :statutAdmin, :statutSubordonne)');
+$req = $bdd->prepare('SELECT * FROM utilisateurs');
 
+$account = $req->fetchAll();
+
+$newcontactdetails = array(
+    'new_nom' => $_POST['nom'],
+    'new_prenom' => $_POST['prenom'],
+    'new_email' => $_POST['email'],
+    'new_password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+);
+
+$bdd ->execute(" UPDATE utilisateurs
+    nom = 'new_nom',
+    prenom = 'new_prenom',
+    email = 'new_email';
+    password = 'new_password';
+    WHERE ID= ?");
 ?>
