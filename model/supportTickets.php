8715f8a42963388f5318e584cfeb47ed906ea73e
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-19 16:39:24 +0200

    udpate

---
+++ model/supportTickets.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Envoi d'un ticket de support.
+ * Le statut du ticket est :
+ *  - 1 : à traiter
+ *  - 2 : en attente
+ *  - 3 : terminé
+ *
+ * La priorité du ticket est :
+ *  - 1 : Haute
+ *  - 2 : Moyenne
+ *  - 3 : Basse
+ * @return bool
+ */
+
+function sendSupportTicket() : bool
+{
+    $bdd = connectBDD();
+    $req = $bdd->prepare('INSERT 
+        INTO ticketsDeSupport(etat, priorite, time, contenu, email) 
+        VALUES(:etat, :priorite, :time, :contenu, :email)');
+
+
+    $req->execute([
+        'etat' => 1,
+        'priorite' => $_POST['priorite'],
+        'email' => $_POST['email'],
+        'time' => date("Y-m-d H:i:s"),
+        'contenu' => $_POST['message']
+    ]);
+
+    return true;
+}
