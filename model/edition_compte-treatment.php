733fa3a7ab3dd4f082da7c03ebeb184e70dcaf02
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-23 17:00:23 +0200

    Modification to edition-compte treatmemt

--- model/edition_compte-treatment.php
+++ model/edition_compte-treatment.php
@@ -1,21 +1,28 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=promethec' , 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-$req = $bdd->prepare('SELECT * FROM utilisateurs');
-$req->execute(array($_SESSION["ID"]));
+/* ------------------------------------BDD -------------------------------------------------------------*/
+$bdd = connectBDD();
+$req = $bdd->prepare('SELECT ID, nom, prenom, email, password FROM utilisateurs');
+$req->execute();
+$account = $req->fetch();
+$_SESSION["userID"] = $account['ID'];
 
-$account = $req->fetchAll();
+/*-----------------------------------fonctions edition de profil----------------------------------------*/
 
-$newcontactdetails = array(
-    'new_nom' => $_POST['nom'],
-    'new_prenom' => $_POST['prenom'],
-    'new_email' => $_POST['email'],
-    'new_password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
-);
 
-$bdd ->execute(" UPDATE utilisateurs
-    nom = 'new_nom',
-    prenom = 'new_prenom',
-    email = 'new_email';
-    password = 'new_password';
-    WHERE ID= ?");
+
+/*$newcontactdetails = array(
+ 'new_nom' => $_POST['nom'],
+ 'new_prenom' => $_POST['prenom'],
+ 'new_email' => $_POST['email'],
+ 'new_password' => password_hash($_POST['password'], PASSWORD_DEFAULT),
+ );
+
+ $bdd ->execute(" UPDATE utilisateurs
+ nom = 'new_nom',
+ prenom = 'new_prenom',
+ email = 'new_email';
+ password = 'new_password';
+ WHERE ID= ?");
+ 
+ */
 ?>
