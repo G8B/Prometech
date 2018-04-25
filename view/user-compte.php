0ecf2dbabd5b08e5d1e6c61af77dae79e1527655
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-26 01:45:38 +0200

    update user-compte.php (edition view)

--- view/user-compte.php
+++ view/user-compte.php
@@ -34,12 +34,12 @@ require('C:/xampp/htdocs/controller/edition_compte-controller.php');
 				
 		<p >
             <label for="nom">Nom :</label>
-            <input type="Nom" name="Nom" id ="newnom" placeholder="<?php echo $userInfos['nom']?>" size="25"/>
+            <input type="text" name="Nom" name ="newnom" placeholder="<?php echo $userInfos['nom']?>" size="25"/>
         </p>
         <br>
         <p >
             <label for="prenom">Prenom :</label>
-            <input type="prenom" name="prenom" id="newprenom" placeholder="<?php echo $userInfos['prenom']?>"   size="25"/>
+            <input type="text" name="prenom" name="newprenom" placeholder="<?php echo $userInfos['prenom']?>"   size="25"/>
         </p>
         <br>
          <!--
@@ -51,24 +51,24 @@ require('C:/xampp/htdocs/controller/edition_compte-controller.php');
         -->
         <p >
             <label for="email">Email :</label>
-            <input type="email" name="email"  placeholder="<?php echo $userInfos['email']?>"  size="25"/>
+            <input type="mail" name="newemail"  placeholder="<?php echo $userInfos['email']?>"  size="25"/>
         </p>
         <br>
         <p >
             <label for="password">Mot de passe actuel :</label>
-            <input type="password" name="password" id="mdpactuel" placeholder="Actual password"  size="25"/>
+            <input type="password" name="mdpactuel" id="mdpactuel" placeholder="Actual password"  size="25"/>
         </p>        
         <br>
         
         <p >
             <label for="newpassword">Nouveau mot de passe:</label>
-            <input type="password" name="newpassword" id="newmdp1" placeholder="New password"  size="25"/>
+            <input type="password" name="newmdp1" id="newmdp1" placeholder="New password"  size="25"/>
         </p>
         <br>
         
         <p >
             <label for="confirm">Confirmer nouveau mot de passe:</label>
-            <input type="password" name="confirmation" id="newmdp2" placeholder="New password"  size="25"/>
+            <input type="password" name="newmdp2" id="newmdp2" placeholder="New password"  size="25"/>
         </p>
         <br>
         <br>
