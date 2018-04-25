9cddbad2e0212c1369cf46081581aa42d8d26231
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-26 00:54:57 +0200

    get the user'ID when login

--- model/signInTreatment.php
+++ model/signInTreatment.php
@@ -4,18 +4,18 @@
 function login($email, $password): bool
 {
     $bdd = connectBDD();
-    $req = $bdd->prepare('SELECT email, password, prenom FROM utilisateurs WHERE email = :email');
+    $req = $bdd->prepare('SELECT ID, email, password, prenom FROM utilisateurs WHERE email = :email');
     $req->execute(['email' => $email]);
     $user = $req->fetch();
-
+    
     if (!$user) {
         return false;
     }
-
+    
     if (password_verify($password, $user['password'])) {
         session_start();
         $_SESSION['prenom'] = $user['prenom'];
-        
+        $_SESSION['userID'] = $user['ID'];
         return true;
     } else {
         return false;
