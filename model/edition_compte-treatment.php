4468159907098026fc50f901d0e2c928f65665e1
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-26 02:53:06 +0200

    updated edition_compte-treatment.php

--- model/edition_compte-treatment.php
+++ model/edition_compte-treatment.php
@@ -31,29 +31,32 @@ $userInfos = $req->fetch();
 
 function updatenom($newnom, $iduser)
 {
-    require('C:/xampp/htdocs/model/connectBDD.php');
+    
+    $bdd=connectBDD();
     $insertnom = $bdd->prepare('UPDATE utilisateurs SET nom = ? WHERE ID = ?');
     $insertnom->execute(array($newnom, $iduser));
 }
 
 function updateprenom($newprenom, $iduser)
 {
-    require('C:/xampp/htdocs/model/connectBDD.php');
+    
+    $bdd=connectBDD();
     $insertprenom = $bdd->prepare('UPDATE utilisateurs SET prenom = ? WHERE ID = ?');
     $insertprenom->execute(array($newprenom, $iduser));
 }
 
 function updatemail($newmail, $iduser)
 {
-    require('C:/xampp/htdocs/model/connectBDD.php');
+    $bdd=connectBDD();
     $insertmail = $bdd->prepare('UPDATE utilisateurs SET email = ? WHERE ID = ?');
     $insertmail->execute(array($newmail, $iduser));
 }
 
 function updatepassword($newmdp, $iduser)
 {
-    require('C:/xampp/htdocs/model/connectBDD.php');
+    $bdd=connectBDD();
     $insertpassword = $bdd->prepare("UPDATE utilisateurs SET password = ? WHERE ID = ?");
     $insertpassword->execute(array($newmdp, $iduser));
 }
+
 ?>
