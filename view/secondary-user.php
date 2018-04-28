c8e2b4275e4b7c2ddb384725bfcf51f5bdab5e9b
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-04-28 18:58:50 +0200

    modified secondary-user view

--- view/secondary-user.php
+++ view/secondary-user.php
@@ -1,3 +1,7 @@
+<?php
+  require('../controller/secondaryUserController.php');
+
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -14,139 +18,153 @@
 <div id="content-window">
     <?php require_once("sidebar.php"); ?>
   
- <link rel="stylesheet" type="text/css" href="/public/css/secondaryUser.css">
+ <link rel="stylesheet" type="text/css" href="/public/css/secondaryUser.css"> 
+ 
  
  <div class="main-wrapper">
-	<div class="secondary-user-board">
-        <div class="accordion-tab">
-            <input id="accordion-tab-one" type="radio" name="accordion" checked>
-            <label for="accordion-tab-one">Enfant 1</label>
-            <div class="accordion-tab-content">
-                <div class="authorized-content">
-                     <table>
-                     	<br>
-                     	<tbody>
-                     		<tr>
-                     		<th>Pieces</th>
-    						<th>Capteurs autorises</th>
-    						
-    						</tr>
-    				
-    						<tr>
-    							<td>Chambre 2</td>
-    							<td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
+	<button class="accordion">Enfant 1</button>
+	<div class="panel">
+		<table>
+                        <br>
+                        <tbody>
+                            <tr>
+                            <th>Pieces</th>
+                            <th>Capteurs autorises</th>
+                            
+                            </tr>
+                    
+                            <tr>
+                                <td>Chambre 2</td>
+                                <td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
 
-    						<tr>
-    							<td>Salon</td>
-    							<td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-    						<tr>
-    							<td>Cuisine</td>
-    							<td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-                     	</tbody>
-                     	
-    				</table><br>
-                   
-                </div>
-            </div>
-        </div>
-        <div class="accordion-tab">
-            <input id="accordion-tab-two" type="radio" name="accordion">
-            <label for="accordion-tab-two">Enfant 2</label>
-            <div class="accordion-tab-content">
-                <div class="authorized-content">
-                    <table>
-                     	<br>
-                     	<tbody>
-                     		<tr>
-                     		<th>Pieces</th>
-    						<th>Capteurs autorises</th>
-    						
-    						</tr>
-    				
-    						<tr>
-    							<td>Chambre 2</td>
-    							<td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-
-    						<tr>
-    							<td>Salon</td>
-    							<td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-    						<tr>
-    							<td>Cuisine</td>
-    							<td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-                     	</tbody>
-                     	
-    				</table><br>
-                </div>
-            </div>
-        </div>
-        <div class="accordion-tab">
-            <input id="accordion-tab-three" type="radio" name="accordion">
-            <label for="accordion-tab-three">Invite</label>
-            <div class="accordion-tab-content">
-                <div class="authorized-content">
-                    <table>
-                     	<br>
-                     	<tbody>
-                     		<tr>
-                     		<th>Pieces</th>
-    						<th>Capteurs autorises</th>
-    						
-    						</tr>
-    				
-    						<tr>
-    							<td>Chambre 2</td>
-    							<td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-
-    						<tr>
-    							<td>Salon</td>
-    							<td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-    						<tr>
-    							<td>Cuisine</td>
-    							<td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
-    							content 5 <br> content 6
-    							</td>
-    						
-    						</tr>
-                     	</tbody>
-                     	
-    				</table><br>
-                </div>
-            </div>
-        </div>
+                            <tr>
+                                <td>Salon</td>
+                                <td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                            <tr>
+                                <td>Cuisine</td>
+                                <td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                        </tbody>
+                        
+                    </table>
+                    <div class = "autorisations" style = "display:flex">
+                    <div class="blocBoutton 1" style = "padding:25px">
+                        <input class='modifAutorisation' type="submit" value="Modifier autorisations"/>
+                    </div>
+                    
+                    <div class="blocBoutton 2" style = "padding:25px">
+                        <input class='suprUtilisateur' type="submit" value="Supprimer utilisateur "/>
+                    </div>
 	</div>
+	
 </div>
+ <button class="accordion">Enfant 2</button>
+	<div class="panel">
+		<table>
+                        <br>
+                        <tbody>
+                            <tr>
+                            <th>Pieces</th>
+                            <th>Capteurs autorises</th>
+                            
+                            </tr>
+                    
+                            <tr>
+                                <td>Chambre 2</td>
+                                <td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+
+                            <tr>
+                                <td>Salon</td>
+                                <td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                            <tr>
+                                <td>Cuisine</td>
+                                <td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                        </tbody>
+                        
+                    </table>
+                    <div class = "autorisations" style = "display:flex">
+                    <div class="blocBoutton 1" style = "padding:25px">
+                        <input class='modifAutorisation' type="submit" value="Modifier autorisations"/>
+                    </div>
+                    
+                    <div class="blocBoutton 2" style = "padding:25px">
+                        <input class='suprUtilisateur' type="submit" value="Supprimer utilisateur "/>
+                    </div>
+	</div>
+	
+</div>
+
+<button class="accordion">Invite 1</button>
+	<div class="panel">
+		<table>
+                        <br>
+                        <tbody>
+                            <tr>
+                            <th>Pieces</th>
+                            <th>Capteurs autorises</th>
+                            
+                            </tr>
+                    
+                            <tr>
+                                <td>Chambre 2</td>
+                                <td>Temperature <br>Lumiere<br> Actionneurs volets <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+
+                            <tr>
+                                <td>Salon</td>
+                                <td>Lumiere <br> Actionneurs verrous <br> Temperature <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                            <tr>
+                                <td>Cuisine</td>
+                                <td>Detecteur de fumee <br> Lumiere  <br> content 4 <br>
+                                content 5 <br> content 6
+                                </td>
+                            
+                            </tr>
+                        </tbody>
+                        
+                    </table>
+                    <div class = "autorisations" style = "display:flex">
+                    <div class="blocBoutton 1" style = "padding:25px">
+                        <input class='modifAutorisation' type="submit" value="Modifier autorisations"/>
+                    </div>
+                    
+                    <div class="blocBoutton 2" style = "padding:25px">
+                        <input class='suprUtilisateur' type="submit" value="Supprimer utilisateur "/>
+                    </div>
+	</div>
+	
+</div>
+
  
 <!-- Sidebar toggle -->
 <script>
@@ -158,6 +176,26 @@
         });
     });
 </script>
+
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+</script>
+
+</div>
+
 </body>
 </html>
  
