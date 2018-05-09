d37c9fa7040bfa320fda9477dc75b8bce7d108d3
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-05-09 09:44:31 +0200

    Modification css de l'edition de compte

--- view/template.php
+++ view/template.php
@@ -5,7 +5,7 @@
     <title>Prometech</title>
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <!-- <link rel="stylesheet" type="text/css" href="/public/css/styles.css"> -->
+    <link rel="stylesheet" type="text/css" href="/public/css/styles.css">
     <link rel="stylesheet" type="text/css" href="/public/css/mon_compte.css">
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
     <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
--- view/user-compte.php
+++ view/user-compte.php
@@ -8,42 +8,42 @@
 		<br>
 				
 		<p >
-            <label for="nom">Nom :</label>
-            <input type="text" name ="newnom" placeholder="<?php echo $userInfos['nom']?>" size="25"/>
+             Nom :
+            <input class="compte" type="text" name ="newnom" placeholder="<?php echo $userInfos['nom']?>" size="25" />
+        </p>
+        <br>
+        
+        <p >
+            Prenom :
+            <input class="compte" type="text" name="newprenom" placeholder="<?php echo $userInfos['prenom']?>"   size="25"/>
+        </p>
+        <br>
+    
+        <p >
+            Email :
+            <input class="compte" type="mail" name="newmail"  placeholder="<?php echo $userInfos['email']?>"  size="25"/>
         </p>
         <br>
         <p >
-            <label for="prenom">Prenom :</label>
-            <input type="text" name="newprenom" placeholder="<?php echo $userInfos['prenom']?>"   size="25"/>
-        </p>
-        <br>
-        <p >
-            <label for="email">Email :</label>
-            <input type="email" name="newmail"  placeholder="<?php echo $userInfos['email']?>"  size="25"/>
-        </p>
-        <br>
-        <p >
-            <label for="password">Mot de passe actuel :</label>
-            <input type="password" name="mdpactuel" placeholder="Actual password"  size="25"/>
+            Mot de passe actuel :
+            <input class="compte" type="password" name="mdpactuel" placeholder="Actual password"  size="25"/>
         </p>        
         <br>
         
         <p >
-            <label for="newpassword">Nouveau mot de passe:</label>
-            <input type="password" name="newmdp1" placeholder="New password"  size="25"/>
+            Nouveau mot de passe:
+            <input class="compte" type="password" name="newmdp1" placeholder="New password"  size="25"/>
         </p>
         <br>
         
         <p >
-            <label for="confirm">Confirmer nouveau mot de passe:</label>
-            <input type="password" name="newmdp2" placeholder="New password"  size="25"/>
-        </p>
-        <br>
-        <br>
-         <p>
-            <input class="form-button" type="submit" value="Confirmer"  style="width: 100px" />
+            Confirmer nouveau mot de passe:
+            <input class="compte" type="password" name="newmdp2" placeholder="New password"  size="25"/>
         </p>
         
+        <div class="bouton">
+        	<button class="form-button">Confirmer</button>
+        </div>
         
 	</form>
 	
