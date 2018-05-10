ad2a3b30ddba94873d4c923959ff145d03db4dac
Marin Godechot <marin.godechot@gmail.com>
2018-05-10 22:40:21 +0200

    commit

--- view/userLogements.php
+++ view/userLogements.php
@@ -26,44 +26,4 @@ foreach ($houses as $house) : ?>
     <?php
     $i++;
 endforeach; ?>
-<div class="user-house-dashboard">
-    <h1 class="user-house-name">Logement 2</h1>
-    <div class="accordion-tab">
-        <input id="accordion2-tab-one" type="radio" name="accordion2" checked>
-        <label for="accordion2-tab-one">Salon</label>
-        <div class="accordion-tab-content">
-            <div class="product-grid">
-                <div class="product-box">Capteur 1</div>
-                <div class="product-box">Actionneur 1</div>
-                <div class="product-box">Capteur 2</div>
-                <div class="product-box">Actionneur 2</div>
-            </div>
-        </div>
-    </div>
-    <div class="accordion-tab">
-        <input id="accordion2-tab-two" type="radio" name="accordion2">
-        <label for="accordion2-tab-two">Chambre Parents</label>
-        <div class="accordion-tab-content">
-            <div class="product-grid">
-                <div class="product-box">Capteur 1</div>
-                <div class="product-box">Actionneur 1</div>
-                <div class="product-box">Capteur 2</div>
-                <div class="product-box">Actionneur 2</div>
-            </div>
-        </div>
-    </div>
-    <div class="accordion-tab">
-        <input id="accordion2-tab-three" type="radio" name="accordion2">
-        <label for="accordion2-tab-three">Chambre Enfants</label>
-        <div class="accordion-tab-content">
-            <div class="product-grid">
-                <div class="product-box">Capteur 1</div>
-                <div class="product-box">Actionneur 1</div>
-                <div class="product-box">Capteur 2</div>
-                <div class="product-box">Actionneur 2</div>
-            </div>
-        </div>
-    </div>
-
-</div>
 
