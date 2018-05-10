e09db8ca4ff19c298a960d46074a25df52f1c61b
Marin Godechot <marin.godechot@gmail.com>
2018-05-10 02:13:29 +0200

    Fix error message at login from template

--- view/template.php
+++ view/template.php
@@ -5,8 +5,13 @@
     <title>Prometech</title>
     <link href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <?php $style = (($title == 'Mon compte')||($_GET['page'] == 'edit-account')) ? "<link rel=\"stylesheet\" type=\"text/css\" href=\"/public/css/mon_compte.css\">" : "<link rel=\"stylesheet\" type=\"text/css\" href=\"/public/css/styles.css\"> ";
-    echo $style; ?>
+    <?php
+    if (isset($userInfos))
+        $style = '<link rel="stylesheet" type="text/css" href="/public/css/mon_compte.css">';
+    else
+        $style = '<link rel="stylesheet" type="text/css" href="/public/css/styles.css">';
+
+    echo $style;?>
     <link rel="stylesheet" type="text/css" href="/public/css/headerAndFooter.css"
     <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
     <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
