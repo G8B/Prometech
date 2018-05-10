4f67476af689682cfcff5c78d6f3a158ce201630
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-05-10 19:30:08 +0200

    ajout css pour ajout de logement

--- view/ajoutLogement.php
+++ view/ajoutLogement.php
@@ -1,32 +1,43 @@
+<link rel="stylesheet" type="text/css" href="/public/css/mon_compte.css">
+
+
 <div class="form_logement">
-    <form method="post" action="">
-
-        <p>
-            <img src="logo.png"/>
+	
+		<form action="" method="post">
+	
+		<h1>Ajouter un logement</h1>
+		<br>
+				
+		<p >
+             Adresse :
+            <input  class ="compte" type="adress" name="adresse" size="50"/>
         </p>
-        <p>
-            <label for="Adresse">Adresse :</label>
-            <input type="adress" name="adresse" size="25"/>
+        <br>
+        
+        <p >
+             Nombres de pieces :
+            <input class="compte" type="number" name="nbrPieces"  min = 1 max = 1000 step = 1 size="50" />
         </p>
-        <p>
-            <label for="Nombre de pieces">Nombres de pièces :</label>
-            <input type="number" name="nbrPieces"  min = 1 max = 1000 step = 1 size="25" />
+        <br>
+    
+        <p >
+             Nombre d'habitants :
+            <input class="compte" type="number" name="nbrHabitants"  min = 1 max = 1000 step = 1 size="50"/>
         </p>
-        <p>
-            <label for="Nombre d'habitants">Nombre d'habitants :</label>
-            <input type="number" name="nbrHabitants"  min = 1 max = 1000 step = 1 size="25"/>
-        </p>
-        <p>
-            <label for="Superficie">Superficie (m²) :</label>
-            <input type="number" name="superficie"  min = 1 max = 10000 step = 1 size="25"/>
-        </p>
+        <br>
+        <p >
+            Superficie :
+            <input class="compte" type="number" name="superficie"  min = 1 max = 10000 step = 1 size="50"/>
+        </p>        
+        <br>
+        
+        <div class="bouton">
+        	<button class="form-button">Confirmer</button>
+        </div>
+        
+	</form>
+	
 
-
-
-
-        <input id='inscription' class="form-button" type="submit" value=" +Ajouter le logement"/>
-    </form>
-
-
-
+	
 </div>
+
