1f20255a34c6868dfc55d9481fc948fc56c4bb4e
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-05-16 13:24:05 +0200

    traitement logs

--- model/logsTreatment.php
+++ model/logsTreatment.php
@@ -3,10 +3,9 @@
 function ajoutLog($commentaire){}
 $bdd= connectBDD();
 $req = $bdd->prepare('INSERT INTO logs(time, ID_utilisateur, action) VALUES (NOW() , :ID_utilisateur, :action)');
-$req->execute(array(
-    'ID_utilisateur'=>$_SESSION['userID'],
-    'action'=>$commentaire
-));
+$req->bindParam(':ID_utilisateur', $_SESSION['userID']);
+$req->bindParam(':action' , $commentaire);
+$req->execute();
 
 function getLogs(){
     $bdd=connectBDD();
