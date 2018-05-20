323ff56e2bec6c4f1ef08c2a0d65f03733ec568a
Eddyah <32392531+Eddyah@users.noreply.github.com>
2018-05-20 11:15:54 +0200

    affichage dynamique des tickets de support

---
+++ public/js/ticketSupport.js
@@ -0,0 +1,31 @@
+/* Get the ticket
+var ticket = document.getElementById('myDIV');
+
+//Get the button
+ var row = document.getElementById("test");
+
+ 
+ row.onclick = function showTicket() {
+	 if (ticket.style.display === "none") {
+	        ticket.style.display = "block";
+	    } else {
+	        ticket.style.display = "none";
+	    }
+ }
+ 
+ */
+var listes = document.getElementsByClassName("Liste_ticket");
+
+for (i =0 ; i< listes.length ; i++){
+	 listes[i].style.display = "block";
+	 }
+
+$(document).ready(function () {
+    $('.text').hide();
+    $('.expander').click(function () {
+        // .parent() selects the A tag, .next() selects the P tag
+        $(this).parent().next().slideToggle(200);
+    });
+    $('.text').slideUp(200);
+});
+
--- view/support.php
+++ view/support.php
@@ -1,32 +1,49 @@
 <link rel="stylesheet" type="text/css" href="/public/css/ticketSupport.css">
 <html>
-	<div class="tableau-support">
-	<table>
-		<tr>
-			<th>ID</th>
-			<th>Priorité</th>
-			<th>Email</th>
-			<th>Objet</th>
-			<th>Statut</th>
-		</tr>
-		<tr>
-			<?php 
-			$Tickets = getTickets();
-
-			foreach ($Tickets as $Ticket){
-			    echo '<tr><td>' . $Ticket['ID'] . '</td>' ;
-			    echo '<td>' . returnPriorite($Ticket['priorite']) . '</td>' ;
-			    echo '<td>' . $Ticket['email'] . '</td>';
-			    echo '<td>'. $Ticket['objet'].'</td>';
-			    echo '<td>'. returnStatut($Ticket['etat']) . '</td></tr>';
-			}
+	<div id="main">
+		<div class="tableau-support">
+			<table>
+				<tr>
+				<th>ID</th>
+				<th>Priorité</th>
+				<th>Email</th>
+				<th>Objet</th>
+				<th>Statut</th>
+				</tr>
+				<tr>
+				<?php 
+			     $Tickets = getTickets();
+			     foreach ($Tickets as $Ticket){
+			         echo '<tr><td>' . $Ticket['ID'] . '</td>' ;
+			         echo '<td class="ticketRow">' . returnPriorite($Ticket['priorite']) . '</td>' ;
+			         echo '<td class="ticketRow">' . $Ticket['email'] . '</td>';
+			         echo '<td class="ticketRow">'. $Ticket['objet'].'</td>';
+			         echo '<td class="ticketRow">'. returnStatut($Ticket['etat']) . '</td></tr>';
+			     }
 		
-		 ?>
+		        ?>
 		
-		</tr>
+				</tr>
 		      
-	</table>
+			</table>
+		</div>
+		<div class = "Liste_ticket">
+			<?php 
+			$i = 0;
+			foreach ($Tickets as $Ticket) : ?>
+			<button class="Tickets"><div class="expander"><?php echo $Ticket['email'] . ' '. $Ticket['time'] ?></div></button>
+			<p class="text"><?php echo $Ticket['contenu'] ?></p>
+			<?php $i++ ;  endforeach;?>
+		</div>
+		
+		<div class="boutons">
+		
+		<button id="answer-button"  class="support-button">Répondre</button>
+		<button id="status-button" class="support-button">Changer statut</button>
+		</div>
 	</div>
+			
+<script type="text/javascript" src="/public/js/ticketSupport.js"></script>
 	
-
+	
 </html>
