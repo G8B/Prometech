f146fd7f1968c5dd1753b522fba00d2f50c6700b
Salome Leduc <salome.leduc@isep.fr>
2018-05-23 10:58:04 +0200

    Ajout lien dans home footer vers CGU + correction de quelques fautes

--- view/home-footer.php
+++ view/home-footer.php
@@ -1,5 +1,5 @@
 <ul id="link-footer">
     <li><a href="/index.php?target=home&page=login">Accueil</a></li>
-    <li><a href="#">CGU</a></li>
+    <li><a href="/index.php?target=home&page=mentions-legales">CGU</a></li>
     <li><a href="/index.php?target=home&page=contact">Contact</a></li>
 </ul>
