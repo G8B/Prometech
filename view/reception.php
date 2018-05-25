bcfbbd638c3e30d2f7712a60f41ae1df89bb398b
Mael <mael.ulienne@gmail.com>
2018-05-25 10:35:15 +0200

    creé le fichier reception.php

---
+++ view/reception.php
@@ -0,0 +1,7 @@
+<div id ="reception">
+    <h1>Welcome ! </h1>
+    <p> Bienvenue sur notre site  <?php echo $_SESSION['userID']?> !</p>
+    <p> Vous avez <?php echo $_SESSION['nbrecapteur']?> capteur actifs  dans l'appartement </p>
+
+</div>
+
