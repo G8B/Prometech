881ded6eaedc76ee26ff8d5434e01be53ee3269e
danykh <dany.khalil97@gmail.com>
2018-05-28 11:55:46 +0200

    edite un logement

--- view/EditerLogement.php
+++ view/EditerLogement.php
@@ -1,10 +1,19 @@
-<div class="update_logement">
+
+
+
+<link rel="stylesheet" href="/public/css/editerLogement.css"/>
+
+
+
+<div>
     <form method="post" action="" id="updateLogement">
 
         <h1>Editer un logement</h1>
 
+
+
         <p>
-            <select title="idHouse" name="idHouse" form="ajoutPiece">
+            <select title="idHouse" name="idHouse" form="updateLogement">
                 <?php foreach ($houses as $house) : ?>
                     <option value="<?php echo $house['ID_logement'] ?>"><?php echo getHouseAdress($house['ID_logement']); ?></option>
                 <?php endforeach; ?>
@@ -12,24 +21,25 @@
         </p>
         <p>
             Nombres de pieces :
-            <input class="compte" type="number" name="nbrPieces"  min = 1 max = 1000 step = 1 size="50" />
+            <input class ="logement" type="number" name="nbrPieces"  min = 1 max = 1000 step = 1 size="50" />
         </p>
         <br>
 
-        <p >
+        <p>
             Nombre d'habitants :
-            <input class="compte" type="number" name="nbrHabitants"  min = 1 max = 1000 step = 1 size="50"/>
+            <input class ="logement" type="number" name="nbrHabitants"  min = 1 max = 1000 step = 1 size="50"/>
         </p>
         <br>
-        <p >
+        <p>
             Superficie :
-            <input class="compte" type="number" name="superficie"  min = 1 max = 10000 step = 1 size="50"/>
+            <input class ="logement" type="number" name="superficie"  min = 1 max = 10000 step = 1 size="50"/>
         </p>
         <br>
 
 
 
 
-        <input id='updateLogement' class="form-button" type="submit" value="Editer son logement"/>
+        <input class ="logement" type="submit" value="Editer son logement"/>
+
     </form>
 </div>
