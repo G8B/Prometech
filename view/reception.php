94ca289a58c90e738e92af88aa002c0f7b4a9291
Mael <mael.ulienne@gmail.com>
2018-06-01 12:02:06 +0200

    créé le fichier CSS relatif à la page de reception

--- view/reception.php
+++ view/reception.php
@@ -1,10 +1,11 @@
-
-<body><div id ="reception">
+<link rel="stylesheet" href="/public/css/reception.css">
+<html>
+<body><div class ="reception">
     <h1>Welcome ! </h1>
-    <p> Bienvenue sur notre site  <?php echo $_SESSION['prenom']?> !</p>
-    <p> Vous avez <?php echo getNumberProducts($_SESSION['userID'])?> produit(s) actif(s)  dans <?php echo getNumberLogements($_SESSION['userID'])?> logement(s) enregistré(s)
+    <p> Bienvenue sur notre site  <?php echo $_SESSION['prenom']?> !</br>
+        Vous avez <?php echo getNumberProducts($_SESSION['userID'])?> produit(s) actif(s)  dans <?php echo getNumberLogements($_SESSION['userID'])?> logement(s) enregistré(s)
     </p>
 
 </div>
-
+</body></html>
 
