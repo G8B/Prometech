8617601a66c6e1cff9c634341726ce300c49b052
Mael <mael.ulienne@gmail.com>
2018-06-04 09:31:18 +0200

    Modifie et finalise la mise en page reception

--- view/reception.php
+++ view/reception.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="/public/css/reception.css">
 <html>
-<body><div class ="reception">
-    <h1>Welcome ! </h1>
+<body><div id ="reception">
+    <h1>Welcome ! </h1></br></br>
     <p> Bienvenue sur notre site  <?php echo $_SESSION['prenom']?> !</br>
-        Vous avez <?php echo getNumberProducts($_SESSION['userID'])?> produit(s) actif(s)  dans <?php echo getNumberLogements($_SESSION['userID'])?> logement(s) enregistré(s)
+        Vous avez <?php echo getNumberProducts($_SESSION['userID'])?> produit(s) actif(s)  dans <?php echo getNumberLogements($_SESSION['userID'])?> logement(s) enregistré(s).
     </p>
 
 </div>
