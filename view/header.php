da4b3d1bf6f6a7b49c535b75816c079115cd4b6b
Marin Godechot <marin.godechot@gmail.com>
2018-06-06 10:51:09 +0200

    user button show dropdown menu

--- view/header.php
+++ view/header.php
@@ -3,8 +3,21 @@
         <img src="/public/images/logopromSideWHITE.png" alt="logoprom Side WHITE" id="whiteLogo"/>
     </a>
     <a id="bars-button" href="#"><i class="fa fa-bars" id="icon-bars"></i></a>
-    <a id="userProfile-button" href="#">
-        <i class="fa fa-user-circle" id="icon-user"></i> <br>
-        <p id="username"><?php echo $_SESSION['prenom'] ?></p>
-    </a>
+    <div id="dropdown-user">
+        <button id="userProfile-button">
+            <i class="fa fa-user-circle" id="icon-user"></i> <br>
+            <p id="username"><?php echo $_SESSION['prenom'] ?></p>
+        </button>
+        <div class="dropdown-content">
+            <div class="triangle"></div>
+            <a href="#">Mon compte</a>
+            <a href="#">Déconnexion</a>
+        </div>
+    </div>
 </div>
+
+<script>
+    $('#userProfile-button').click(function () {
+        $('.dropdown-content').toggle();
+    });
+</script>
