9b3a937f668e9e4bd96afcd73c46eacf1c39b6d2
Marin Godechot <marin.godechot@gmail.com>
2018-06-06 10:04:46 +0200

    main logo redirect to home page

--- view/header.php
+++ view/header.php
@@ -1,5 +1,5 @@
 <div class="main-header">
-    <a href="/index.php?target=home&page=login" id="logo">
+    <a href="/index.php?target=user" id="logo">
         <img src="/public/images/logopromSideWHITE.png" alt="logoprom Side WHITE" id="whiteLogo"/>
     </a>
     <a id="bars-button" href="#"><i class="fa fa-bars" id="icon-bars"></i></a>
