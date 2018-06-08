3b53574231c76a36a1100887f9dc56936d2f14f0
Marin Godechot <marin.godechot@gmail.com>
2018-06-08 09:46:34 +0200

    change Domisep in Prométech in user home page

--- view/reception.php
+++ view/reception.php
@@ -3,7 +3,7 @@
 <body>
 <div id="reception">
     <h1>Bonjour <?php echo $_SESSION['prenom'] ?>! </h1><br>
-    <p> Bienvenue sur votre interface Domisep !<br>
+    <p> Bienvenue sur votre interface Prométech !<br>
         Vous avez <?php echo getNumberProducts($_SESSION['userID']) ?> produit(s) actif(s)
         dans <?php echo getNumberLogements($_SESSION['userID']) ?> logement(s) enregistré(s).
     </p>
