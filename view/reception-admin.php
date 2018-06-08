311f9c193283f8f661db78438d23b274c600044c
Marin Godechot <marin.godechot@gmail.com>
2018-06-08 10:11:51 +0200

    ajoute la page d'accueil administrateur

---
+++ view/reception-admin.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="/public/css/reception.css">
+<html>
+<body>
+<div id="reception">
+    <h1>Bonjour <?php echo $_SESSION['prenom'] ?> ! </h1><br>
+    <p> Bienvenue sur votre interface administrateur Prométech !<br>
+    </p>
+</div>
+</body>
+</html>
